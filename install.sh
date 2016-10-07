408c4b898d30411fedc117e1e55a1f7b30384867
Timothy <ygmpkk@gmail.com>
2016-10-07 10:23:20 +0800

    更新脚本安装的git地址

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ echo "备份已有的vim配置"
 mv -f ~/.vim ~/.vim_old2
 
 echo "从Github克隆配置"
-git clone https://github.com/ygmpkk/vimconfig ~/.vim
+git clone https://github.com/ygmpkk/koine-vim ~/.vim
 ln -sf ~/.vim/vimrc ~/.vimrc
 
 echo "正在安装Vim插件..." > ~/.vimprocess
